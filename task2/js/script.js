5a3aa5f3d91dedf9a91caf858aa0dea8736f1142
alex-sky <alexandr.skyrda@gmail.com>
2020-05-24 22:54:57 +0300

    mod script

--- task2/js/script.js
+++ task2/js/script.js
@@ -1,28 +1,73 @@
 'use strict';
 
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
+document.addEventListener('DOMContentLoaded', () => {
 
-const ads = document.querySelectorAll('.promo__adv img');
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
 
-ads.forEach(item => {
-    item.remove();
-});
+    const ads = document.querySelectorAll('.promo__adv img'),
+          promoList = document.querySelector('.promo__interactive-list'),
+          addForm = document.querySelector('form.add'),
+          inputFilm = addForm.querySelector('.adding__input'),
+          favorite = addForm.querySelector('[type="checkbox"]');
 
-document.querySelector('.promo__genre').innerHTML = 'драма';
+    ads.forEach(item => {
+        item.remove();
+    });
 
-document.querySelector('.promo__bg').style.backgroundImage = 'url(img/bg.jpg)';
+    document.querySelector('.promo__genre').innerHTML = 'драма';
 
-movieDB.movies.sort().forEach((item, i) => {
-    const promoItem = document.createElement('li');
-    promoItem.classList.add('promo__interactive-item');
-    promoItem.innerHTML = `${i+1}. ${item} <div class="delete"></div>`;
-    document.querySelector('.promo__interactive-list').append(promoItem);
+    document.querySelector('.promo__bg').style.backgroundImage = 'url(img/bg.jpg)';
+    
+    const addFilm = () => {
+        promoList.innerHTML = '';
+        movieDB.movies.sort();
+
+        movieDB.movies.forEach((item, i) => {
+            const promoItem = document.createElement('li');
+            promoItem.classList.add('promo__interactive-item');
+            promoItem.innerHTML = `${i+1}. ${item} <div class="delete"></div>`;
+            promoList.append(promoItem);
+        });
+
+        document.querySelectorAll('.delete').forEach((btn, i) => {
+            btn.addEventListener('click', () => {
+                btn.parentElement.remove();
+                movieDB.movies.splice(i, 1);
+                addFilm();
+            });
+        });
+    };
+
+    addForm.addEventListener('submit', e => {
+        e.preventDefault();
+
+        let film = inputFilm.value;
+        if (!film) {
+            alert('Заполните поле');
+            return false;
+        } else if ( film.length > 21 ) {
+            film = `${film.substring(0, 22)}...`;
+        }
+
+        if (favorite.checked) {
+            console.log('favorite');
+        }
+
+        movieDB.movies.push(film);
+
+        addFilm();
+
+        e.target.reset();
+    });
+
+    addFilm();
+    
 });
